aea2ee1fd3a14b812a993c2ef6121da4f61f5b01
jesse <szx961102@gmail.com>
2023-02-20 15:34:26 +0800

    add forgert psw

---
+++ src/routes/forgetPsw/+page.svelte
@@ -0,0 +1,14 @@
+<div class="p-5 ">
+	<label for="phone">手机号</label>
+	<input type="text" name="phone" id="phone" class="input " />
+	<label for="code">验证码</label>
+	<div class="flex gap-2">
+		<input type="text" placeholder="请输入验证码" class="input" />
+		<button class="btn variant-soft-primary">获取验证码</button>
+	</div>
+	<label for="newPsw">新密码</label>
+	<input type="password" name="newPsw" id="newPsw" class="input-group" />
+	<label for="newPsw">重复新密码</label>
+	<input type="password" name="newPsw" id="newPsw" class="input-group" />
+	<button class="btn variant-filled-primary w-full mt-5">确认</button>
+</div>
