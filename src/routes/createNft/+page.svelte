eeee22b9deb3f9b4ad666897ca8ae62000797e94
jesse <szx961102@gmail.com>
2023-02-20 12:54:31 +0800

    add createNft

--- src/routes/createNft/+page.svelte
+++ src/routes/createNft/+page.svelte
@@ -1,1 +1,29 @@
-铸造藏品
+<script>
+	import { FileButton, FileDropzone } from '@skeletonlabs/skeleton';
+	import { ImagePlus } from 'lucide-svelte';
+</script>
+
+<div class="p-5">
+	<div>铸造藏品</div>
+	<form method="post ">
+		<FileDropzone name="files">
+			<svelte:fragment slot="lead">
+				<ImagePlus class="mx-auto" />
+			</svelte:fragment>
+			<svelte:fragment slot="message">上传藏品</svelte:fragment>
+		</FileDropzone>
+		<label class="label">
+			<span>名称</span>
+			<input class="input py-2" />
+		</label>
+		<label class="label">
+			<span>发行数量</span>
+			<input class="input py-2" type="number" />
+		</label>
+		<label class="label">
+			<span>描述</span>
+			<input class="textarea py-10" />
+		</label>
+		<button class="btn variant-filled-secondary w-full mt-5 ">提交</button>
+	</form>
+</div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,7 +8,10 @@ const config = {
         extend: {}
     },
 
-    plugins: [...require('@skeletonlabs/skeleton/tailwind/skeleton.cjs')()]
+    plugins: [
+        require('@tailwindcss/forms'),
+    ...require('@skeletonlabs/skeleton/tailwind/skeleton.cjs')()
+]
 };
 
 module.exports = config;
