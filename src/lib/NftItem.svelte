040e2028bdce5b1a865b27b5d96535ac4244dfde
jesse <szx961102@gmail.com>
2023-02-20 14:44:25 +0800

    add nft detail

--- src/lib/NftItem.svelte
+++ src/lib/NftItem.svelte
@@ -7,10 +7,12 @@
 </script>
 
 <div class="bg-surface-50 rounded-lg">
-	<img {src} alt="nft" class="w-full aspect-square " />
-	<div class="px-3 pb-2">
-		<div class="pt-2">{name}</div>
-		<div class="mt-4 mb-1 text-xs"><span class="text-red-400">#{number}/</span>{all}</div>
-		<div>￥{price}</div>
-	</div>
+	<a href="/nftDetail" class="unstyled">
+		<img {src} alt="nft" class="w-full aspect-square " />
+		<div class="px-3 pb-2">
+			<div class="pt-2">{name}</div>
+			<div class="mt-4 mb-1 text-xs"><span class="text-red-400">#{number}/</span>{all}</div>
+			<div>￥{price}</div>
+		</div>
+	</a>
 </div>
---
+++ src/routes/(NFT)/nftDetail/+page.svelte
@@ -0,0 +1,21 @@
+<img
+	src="https://static.ibox.art/file/oss/test/image/nft-goods/167f08459056401d8206cfc80b4e799e.png?style=st6"
+	alt=""
+	class="w-full"
+/>
+<div class="bg-surface-50 px-5 pt-4 pb-2">
+	<div class="text-xs">该作品拥有官方认证</div>
+	<div class="text-2xl font-bold mt-2">凌霄殿#413</div>
+</div>
+
+<div class="bg-surface-50 px-5 mt-2 py-2">
+	<h2 class="text-lg">简介</h2>
+	<div>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum aspernatur officia corporis
+		incidunt ad, temporibus, eum dicta atque sed molestiae quis corrupti quod mollitia possimus
+		debitis ab, ullam ea ratione.
+	</div>
+</div>
+<div>
+	<button class="btn variant-filled-secondary absolute bottom-0 inset-x-5">￥999 购买 </button>
+</div>
