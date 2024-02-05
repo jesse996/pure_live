399e6feb5a2a0b5b44687b92a3406da9471c3091
jesse <szxfml@gmail.com>
2024-02-05 11:14:12 +0800

    update dnd

---
+++ app/routes/dnd.tsx
@@ -0,0 +1,6 @@
+import { DndList } from "~/components/DndLsit/DndList";
+import { DndKitDemo } from "~/components/DndKitDemo";
+
+export default function Dnd() {
+  return <DndKitDemo />;
+}
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,6 @@
 export default {
   plugins: {
-    tailwindcss: {},
+    // tailwindcss: {},
     "postcss-preset-mantine": {},
     "postcss-simple-vars": {
       variables: {
