d7e791a4ab8b361fd76555dc2954bee4d03f049e
jesse <szxfml@gmail.com>
2024-02-15 12:18:33 +0800

    update dep

--- app/utils/index.ts
+++ app/utils/index.ts
@@ -1,10 +1,10 @@
-import { createDirectus, rest } from "@directus/sdk";
+// import { createDirectus, rest } from "@directus/sdk";
 import { createClient } from "@supabase/supabase-js";
 import { Database } from "~/types/supabase";
 
-export const client = createDirectus("http://jesse.x3322.net:8055").with(
-  rest()
-);
+// export const client = createDirectus("http://jesse.x3322.net:8055").with(
+//   rest()
+// );
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
