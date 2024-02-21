6902846b282236e4fc06a728e2c553c2608f00a7
jesse <szxfml@gmail.com>
2024-02-21 16:53:49 +0800

    update remix

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,10 @@
 import {
-  unstable_vitePlugin as remix,
-  unstable_cloudflarePreset as cloudflare,
+  vitePlugin as remix,
+  cloudflareDevProxyVitePlugin as remixCloudflareDevProxy,
 } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [
-    remix({
-      presets: [cloudflare()],
-    }),
-    tsconfigPaths(),
-  ],
-  ssr: {
-    resolve: {
-      externalConditions: ["workerd", "worker"],
-    },
-  },
+  plugins: [remixCloudflareDevProxy(), remix(), tsconfigPaths()],
 });
