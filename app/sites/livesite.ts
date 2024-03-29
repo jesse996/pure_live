e649140e211270ddc65f9946cec396ddfb6e4883
jesse <szxfml@gmail.com>
2024-03-30 03:06:21 +0800

    add bilibili class

--- app/sites/livesite.ts
+++ app/sites/livesite.ts
@@ -1,84 +1,84 @@
 import {
-  LiveStatus,
-  type LiveArea,
-  type LiveCategory,
-  type LiveCategoryResult,
-  type LiveDanmaku,
-  type LivePlayQuality,
-  type LiveRoom,
-} from '~/types/live'
-import type { LiveSuperChatMessage } from '~/types/live/liveMessage'
+	type LiveArea,
+	type LiveCategory,
+	type LiveCategoryResult,
+	type LiveDanmaku,
+	type LivePlayQuality,
+	type LiveRoom,
+	LiveStatus,
+} from "~/types/live";
+import type { LiveSuperChatMessage } from "~/types/live/liveMessage";
 import type {
-  LiveSearchAnchorResult,
-  LiveSearchRoomResult,
-} from '~/types/live/liveSearchResult'
+	LiveSearchAnchorResult,
+	LiveSearchRoomResult,
+} from "~/types/live/liveSearchResult";
 
-class LiveSite {
-  id = ''
-  name = ''
+export class LiveSite {
+	id = "";
+	name = "";
 
-  getDanmaku(): LiveDanmaku {
-    return {} as LiveDanmaku
-  }
+	getDanmaku(): LiveDanmaku {
+		return {} as LiveDanmaku;
+	}
 
-  async getCategores(page: number, pageSize: number): Promise<LiveCategory[]> {
-    return []
-  }
+	async getCategores(page: number, pageSize: number): Promise<LiveCategory[]> {
+		return [];
+	}
 
-  async searchRooms(keyword: string, page = 1): Promise<LiveSearchRoomResult> {
-    return { hasMore: false, items: [] }
-  }
+	async searchRooms(keyword: string, page = 1): Promise<LiveSearchRoomResult> {
+		return { hasMore: false, items: [] };
+	}
 
-  async searchAnchors(
-    keyword: string,
-    page = 1
-  ): Promise<LiveSearchAnchorResult> {
-    return { hasMore: false, items: [] }
-  }
+	async searchAnchors(
+		keyword: string,
+		page = 1,
+	): Promise<LiveSearchAnchorResult> {
+		return { hasMore: false, items: [] };
+	}
 
-  async getCategoryRooms(
-    category: LiveArea,
-    page = 1
-  ): Promise<LiveCategoryResult> {
-    return { hasMore: false, items: [] }
-  }
+	async getCategoryRooms(
+		category: LiveArea,
+		page = 1,
+	): Promise<LiveCategoryResult> {
+		return { hasMore: false, items: [] };
+	}
 
-  async getRecommendRooms(page = 1): Promise<LiveCategoryResult> {
-    return { hasMore: false, items: [] }
-  }
+	async getRecommendRooms(page = 1): Promise<LiveCategoryResult> {
+		return { hasMore: false, items: [] };
+	}
 
-  async getRoomDetail(roomId: string): Promise<LiveRoom> {
-    return {
-      cover: '',
-      platform: '',
-      watching: '0',
-      roomId: '',
-      status: false,
-      liveStatus: LiveStatus.offline,
-      title: '',
-      link: '',
-      avatar: '',
-      nick: '',
-      isRecord: false,
-    }
-  }
+	async getRoomDetail(roomId: string): Promise<LiveRoom> {
+		return {
+			cover: "",
+			platform: "",
+			watching: "0",
+			roomId: "",
+			status: false,
+			liveStatus: LiveStatus.offline,
+			title: "",
+			link: "",
+			avatar: "",
+			nick: "",
+			isRecord: false,
+		};
+	}
 
-  async getPlayQualites(detail: LiveRoom): Promise<LivePlayQuality[]> {
-    return []
-  }
+	async getPlayQualites(detail: LiveRoom): Promise<LivePlayQuality[]> {
+		return [];
+	}
 
-  async getPlayUrls(
-    detail: LiveRoom,
-    quality: LivePlayQuality
-  ): Promise<string[]> {
-    return []
-  }
+	async getPlayUrls(
+		detail: LiveRoom,
+		quality: LivePlayQuality,
+	): Promise<string[]> {
+		return [];
+	}
 
-  async getLiveStatus(roomId: string): Promise<boolean> {
-    return false
-  }
+	async getLiveStatus(roomId: string): Promise<boolean> {
+		return false;
+	}
 
-  async getSuperChatMessage(roomId: string): Promise<LiveSuperChatMessage[]> {
-    return []
-  }
+	async getSuperChatMessage(roomId: string): Promise<LiveSuperChatMessage[]> {
+		return [];
+	}
 }
--- app/types/live/liveMessage.ts
+++ app/types/live/liveMessage.ts
@@ -1,89 +1,89 @@
 enum LiveMessageType {
-  // 聊天
-  chat = 'chat',
+	// 聊天
+	chat = "chat",
 
-  // 礼物,暂时不支持
-  gift = 'gift',
+	// 礼物,暂时不支持
+	gift = "gift",
 
-  // 在线人数
-  online = 'online',
+	// 在线人数
+	online = "online",
 
-  // 醒目留言
-  superChat = 'superChat',
+	// 醒目留言
+	superChat = "superChat",
 }
 
 export type LiveMessage = {
-  // 消息类型
-  type: LiveMessageType
+	// 消息类型
+	type: LiveMessageType;
 
-  // 用户名
-  userName: string
+	// 用户名
+	userName: string;
 
-  // 信息
-  message: string
+	// 信息
+	message: string;
 
-  // 数据
-  // 单Type=Online时，Data为人气值(long)
-  data: any
+	// 数据
+	// 单Type=Online时，Data为人气值(long)
+	data: any;
 
-  // 弹幕颜色
-  color: LiveMessageColor
-}
+	// 弹幕颜色
+	color: LiveMessageColor;
+};
 
 class LiveMessageColor {
-  r: number
-  g: number
-  b: number
+	r: number;
+	g: number;
+	b: number;
 
-  constructor(r: number, g: number, b: number) {
-    this.r = r
-    this.g = g
-    this.b = b
-  }
+	constructor(r: number, g: number, b: number) {
+		this.r = r;
+		this.g = g;
+		this.b = b;
+	}
 
-  static get white() {
-    return new LiveMessageColor(255, 255, 255)
-  }
+	static get white() {
+		return new LiveMessageColor(255, 255, 255);
+	}
 
-  static numberToColor(intColor: number) {
-    let obj = intColor.toString(16)
+	static numberToColor(intColor: number) {
+		let obj = intColor.toString(16);
 
-    let color = LiveMessageColor.white
-    if (obj.length === 4) {
-      obj = `00${obj}`
-    }
-    if (obj.length === 6) {
-      const R = Number.parseInt(obj.substring(0, 2), 16)
-      const G = Number.parseInt(obj.substring(2, 4), 16)
-      const B = Number.parseInt(obj.substring(4, 6), 16)
+		let color = LiveMessageColor.white;
+		if (obj.length === 4) {
+			obj = `00${obj}`;
+		}
+		if (obj.length === 6) {
+			const R = Number.parseInt(obj.substring(0, 2), 16);
+			const G = Number.parseInt(obj.substring(2, 4), 16);
+			const B = Number.parseInt(obj.substring(4, 6), 16);
 
-      color = new LiveMessageColor(R, G, B)
-    }
-    if (obj.length === 8) {
-      const R = Number.parseInt(obj.substring(2, 4), 16)
-      const G = Number.parseInt(obj.substring(4, 6), 16)
-      const B = Number.parseInt(obj.substring(6, 8), 16)
-      //let A = parseInt(obj.substring(0, 2), 16);
-      color = new LiveMessageColor(R, G, B)
-    }
+			color = new LiveMessageColor(R, G, B);
+		}
+		if (obj.length === 8) {
+			const R = Number.parseInt(obj.substring(2, 4), 16);
+			const G = Number.parseInt(obj.substring(4, 6), 16);
+			const B = Number.parseInt(obj.substring(6, 8), 16);
+			//let A = parseInt(obj.substring(0, 2), 16);
+			color = new LiveMessageColor(R, G, B);
+		}
 
-    return color
-  }
+		return color;
+	}
 
-  toString() {
-    return `#${this.r.toString(16).padStart(2, '0')}${this.g
-      .toString(16)
-      .padStart(2, '0')}${this.b.toString(16).padStart(2, '0')}`
-  }
+	toString() {
+		return `#${this.r.toString(16).padStart(2, "0")}${this.g
+			.toString(16)
+			.padStart(2, "0")}${this.b.toString(16).padStart(2, "0")}`;
+	}
 }
 
 export type LiveSuperChatMessage = {
-  userName: string
-  face: string
-  message: string
-  price: number
-  startTime: Date
-  endTime: Date
-  backgroundColor: string
-  backgroundBottomColor: string
-}
+	userName: string;
+	face: string;
+	message: string;
+	price: number;
+	startTime: Date;
+	endTime: Date;
+	backgroundColor: string;
+	backgroundBottomColor: string;
+};
